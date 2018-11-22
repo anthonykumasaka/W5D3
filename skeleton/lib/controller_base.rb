2177790093cf4038b427719483fe8de1736bac8e
anthonykumasaka <anthonykumasaka@gmail.com>
2018-11-21 16:28:50 -0800

    Add updated W5D3 project

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -7,27 +7,37 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
-    @req = req 
-    @res = res 
+  def initialize(req, res, route_params = {})
+    @req, @res = req, res
+    @params = route_params.merge(req.params)
+    @already_built_response = false
+    @@protect_from_forgery ||= false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
-  def redirect_to(url, status_code = 302)
-    @res.status = status_code
-    @res[location] = url 
-  end
+  def redirect_to(url)
+    prepare_render_or_redirect
 
+    @res.status = 302
+    @res["Location"] = url
+
+    nil
+  end
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
-    @res['Content-Type'] = content_type
+    prepare_render_or_redirect
+
     @res.write(content)
+    @res['Content-Type'] = content_type
+
+    nil
   end
 
   # use ERB and binding to evaluate templates
